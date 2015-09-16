43db81d992f7560aed853b5c1c537019a95933ff
Chang Kon Han <chang_kon@hotmail.com>
2015-09-16 13:00:02 +1200

    Added dto package. Added initial Viewer dto object. Currently contains no implementation

---
+++ src/main/java/changkon/imj/dto/Viewer.java
@@ -0,0 +1,12 @@
+package changkon.imj.dto;
+
+/**
+ * Movie viewer. Viewer is entity which watches movie.
+ * 
+ * This is DTO implementation. Contains less information to be included in XML to reduce
+ * bandwidth.
+ * @author Chang Kon Han
+ */
+public class Viewer {
+
+}
